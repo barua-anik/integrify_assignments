ce6485d201fae82a4329545bd6bcac550cfb3629
anik511580 <engr.anikbarua@gmail.com>
2019-09-10 13:31:32 +0300

    exam

---
+++ exam1_computer/run_me.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+touch "$1_computer_exam1_try2_solution_1.py"
+touch"$1_computer_exam1_try2_solution_2.py"
+touch "$1_computer_exam1_try2_solution_3.py"
+touch "$1_computer_exam1_try2_solution_4.py"
+touch "$1_computer_exam1_try2_solution_5.py"
